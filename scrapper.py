76ce3079fd733d3f2f44d2e556fb892a88961984
Ruven3344 <ruven.peterhans@bluewin.ch>
2023-01-14 23:58:30 +0100

    Python Scraper

---
+++ scrapper.py
@@ -0,0 +1,35 @@
+import praw
+from praw.models import MoreComments
+import pandas as pd
+
+reddit = praw.Reddit(client_id='I-vu87MMWbuDgjqWJQOANw',
+                     client_secret='I4KVCO9jE7R082zKQHm7rXcLfYgKZg', user_agent='Scraping')
+
+subreddit = reddit.subreddit("Python")
+
+content_dict = {"Title": [], "Author": [], "ID": [], "Body": [], "Post": []}
+
+for post in subreddit.top("month"):
+    content_dict["Title"].append(post.title)
+    content_dict["Author"].append(post.author)
+    content_dict["ID"].append(post.id)
+#    content_dict["URL"].append(post.url)
+    content_dict["Body"].append(post.selftext)
+    content_dict["Post"].append(True)
+"""
+    # extracting comments
+    post_url = "https://www.reddit.com" + post.permalink
+    submission = reddit.submission(url = post_url)
+    for comment in submission.comments:
+        if type(comment) == MoreComments:
+            continue
+        content_dict["Title"].append(0)
+        content_dict["Author"].append(comment.author)
+        content_dict["ID"].append(comment.id)
+        comment_url = "https://www.reddit.com" + comment.permalink
+        content_dict["URL"].append(comment_url)
+        content_dict["Body"].append(comment.body)
+        content_dict["Post"].append(False)
+"""
+content_df = pd.DataFrame.from_dict(content_dict, orient='columns')
+content_df.to_csv("Content_data.csv")
