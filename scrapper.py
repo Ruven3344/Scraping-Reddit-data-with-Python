7727d77179da4239af22b056d4b1b20c12317384
ruven <ruven.peterhans@bluewin.ch>
2023-01-16 10:26:59 +0100

    excel problem solved

--- scrapper.py
+++ scrapper.py
@@ -7,19 +7,20 @@ reddit = praw.Reddit(client_id='I-vu87MMWbuDgjqWJQOANw',
 
 subreddit = reddit.subreddit("Python")
 
-content_dict = {"Title": [], "Author": [], "ID": [], "Body": [], "Post": []}
+content_dict = {"Title": [], "Author": [], "URL": [], "ID": [], "Body": [], "Post": []}
 
+# extracting posts
 for post in subreddit.top("month"):
     content_dict["Title"].append(post.title)
     content_dict["Author"].append(post.author)
     content_dict["ID"].append(post.id)
-#    content_dict["URL"].append(post.url)
     content_dict["Body"].append(post.selftext)
     content_dict["Post"].append(True)
-"""
-    # extracting comments
+
+    # extracting comments of each post
     post_url = "https://www.reddit.com" + post.permalink
-    submission = reddit.submission(url = post_url)
+    content_dict["URL"].append(post_url)
+    submission = reddit.submission(url=post_url)
     for comment in submission.comments:
         if type(comment) == MoreComments:
             continue
@@ -30,6 +31,7 @@ for post in subreddit.top("month"):
         content_dict["URL"].append(comment_url)
         content_dict["Body"].append(comment.body)
         content_dict["Post"].append(False)
-"""
+
 content_df = pd.DataFrame.from_dict(content_dict, orient='columns')
 content_df.to_csv("Content_data.csv")
+
